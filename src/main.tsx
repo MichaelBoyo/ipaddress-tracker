1ce23c7a328f04ab98b767ca2ba36e81d326b830
michaelboyo <103725264+MichaelBoyo@users.noreply.github.com>
2023-09-03 15:36:23 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import { AuthProvider } from "react-oidc-context";
 
-const oidcConfig = {
-  authority: "http://localhost:8080/realms/SpringBootKeycloak",
-  client_id: "login-app",
-  redirect_uri: "http://localhost:5173",
-  onSigninCallback: () => {
-    window.history.replaceState({}, document.title, window.location.pathname);
-  },
-};
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <AuthProvider {...oidcConfig}>
+
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </AuthProvider>
+
 );
